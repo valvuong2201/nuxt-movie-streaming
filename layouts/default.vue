1395e4d3bd64cba6323646a99a568aa91021a47d
King <valvuong2201@gmail.com>
2022-04-03 15:10:03 +0700

    start project

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app">
+    <nuxt keep-alive />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: "DefaultLayout"
+})
+</script>
